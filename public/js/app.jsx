10d75a376848e20d08e2f431c787ef62d6c11988
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2016-11-16 11:06:30 +0100

    add category dropdown to form, handel filter for category

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -8,37 +8,52 @@ var app = app || {};
 (function () {
 	'use strict';
 
-	app.ALL_TODOS = 'all';
-	app.ACTIVE_TODOS = 'active';
-	app.COMPLETED_TODOS = 'completed';
-	var TodoFooter = app.TodoFooter;
-	var TodoItem = app.TodoItem;
+    app.ALL_TODOS = 'all';
+    app.ACTIVE_TODOS = 'active';
+    app.COMPLETED_TODOS = 'completed';
+    var TodoFooter = app.TodoFooter;
+    var TodoItem = app.TodoItem;
+    var TodoCategory = app.TodoCategory;
 
-	var ENTER_KEY = 13;
+    app.CAT_WORK = 'work';
+    app.CAT_PERSONAL = 'personal';
+    app.CAT_SUPERPERSONAL = 'superpersonal';
 
-	var TodoApp = React.createClass({
-		getInitialState: function () {
-			return {
-				nowShowing: app.ALL_TODOS,
-				editing: null,
-				newTodo: ''
-			};
-		},
+    
+    var ENTER_KEY = 13;
 
-		componentDidMount: function () {
-			var setState = this.setState;
-			var router = Router({
-				'/': setState.bind(this, {nowShowing: app.ALL_TODOS}),
-				'/active': setState.bind(this, {nowShowing: app.ACTIVE_TODOS}),
-				'/completed': setState.bind(this, {nowShowing: app.COMPLETED_TODOS})
-			});
-			router.init('/');
-		},
+    var TodoApp = React.createClass({
+        getInitialState: function () {
+            return {
+                nowShowing: app.ALL_TODOS,
+                editing: null,
+                newTodo: '',
+                newCategory: ''
+            };
+        },
+
+        componentDidMount: function () {
+            var setState = this.setState;
+            var router = Router({
+                '/': setState.bind(this, {nowShowing: app.ALL_TODOS}),
+                '/active': setState.bind(this, {nowShowing: app.ACTIVE_TODOS}),
+                '/completed': setState.bind(this, {nowShowing: app.COMPLETED_TODOS}),
+                // Add routes for categories
+        		'/category_work': setState.bind(this, {nowShowing: app.CAT_WORK}),
+        		'/category_personal': setState.bind(this, {nowShowing: app.CAT_PERSONAL}),
+        		'/category_superpersonal': setState.bind(this, {nowShowing: app.CAT_SUPERPERSONAL}),
+        		'/all_categories': setState.bind(this, {nowShowing: app.ALL_TODOS})
+            });
+            router.init('/');
+        },
 
 		handleChange: function (event) {
 			this.setState({newTodo: event.target.value});
 		},
 
+        handleCategoryChange: function (event) {
+            this.setState({newCategory: event.target.value});
+        },
 		handleNewTodoKeyDown: function (event) {
 			if (event.keyCode !== ENTER_KEY) {
 				return;
@@ -46,13 +61,14 @@ var app = app || {};
 
 			event.preventDefault();
 
-			var val = this.state.newTodo.trim();
+            var val = this.state.newTodo.trim();
+            var cat = this.state.newCategory.trim();
 
-			if (val) {
-				this.props.model.addTodo(val);
-				this.setState({newTodo: ''});
-			}
-		},
+            if (val, cat) {
+                this.props.model.addTodo(val, cat);
+                this.setState({newTodo: '', newCategory: ''});
+            }
+        },
 
 		toggleAll: function (event) {
 			var checked = event.target.checked;
@@ -71,10 +87,10 @@ var app = app || {};
 			this.setState({editing: todo.id});
 		},
 
-		save: function (todoToSave, text) {
-			this.props.model.save(todoToSave, text);
-			this.setState({editing: null});
-		},
+        save: function (todoToSave, text, cat) {
+            this.props.model.save(todoToSave, text, cat);
+            this.setState({editing: null});
+        },
 
 		cancel: function () {
 			this.setState({editing: null});
@@ -84,21 +100,30 @@ var app = app || {};
 			this.props.model.clearCompleted();
 		},
 
-		render: function () {
-			var footer;
-			var main;
-			var todos = this.props.model.todos;
-
-			var shownTodos = todos.filter(function (todo) {
-				switch (this.state.nowShowing) {
-				case app.ACTIVE_TODOS:
-					return !todo.completed;
-				case app.COMPLETED_TODOS:
-					return todo.completed;
-				default:
-					return true;
-				}
-			}, this);
+        render: function () {
+            var footer;
+            var category;
+            var main;
+            var todos = this.props.model.todos;
+            var shownTodos = todos.filter(function (todo) {
+                switch (this.state.nowShowing) {
+                case app.ACTIVE_TODOS:
+                    return !todo.completed;
+                case app.COMPLETED_TODOS:
+                    return todo.completed;
+                case app.CAT_WORK:
+                    if(todo.category=='work') return true;
+                    else return false;
+                case app.CAT_PERSONAL:
+                    if(todo.category=='personal') return true;
+                    else return false;
+                case app.CAT_SUPERPERSONAL:
+                    if(todo.category=='superpersonal') return true;
+                    else return false;
+                default:
+                    return true;
+                }
+            }, this);
 
 			var todoItems = shownTodos.map(function (todo) {
 				return (
@@ -132,6 +157,13 @@ var app = app || {};
 			}
 
 			if (todos.length) {
+        category =
+        <TodoCategory
+                        nowShowing={this.state.nowShowing}
+                    />;
+      }
+
+            if (todos.length) {
 				main = (
 					<section className="main">
 						<input
@@ -147,25 +179,37 @@ var app = app || {};
 				);
 			}
 
-			return (
-				<div>
-					<header className="header">
-						<h1>todos</h1>
-						<input
-							className="new-todo"
-							placeholder="What needs to be done?"
-							value={this.state.newTodo}
-							onKeyDown={this.handleNewTodoKeyDown}
-							onChange={this.handleChange}
-							autoFocus={true}
-						/>
-					</header>
-					{main}
-					{footer}
-				</div>
-			);
-		}
-	});
+            return (
+                <div>
+                    <header className="header">
+                        <h1>todos</h1>
+                        <form onKeyDown={this.handleNewTodoKeyDown}>
+                            <input
+                                className="new-todo"
+                                placeholder="What needs to be done?"
+                                value={this.state.newTodo}
+
+
+                                onChange={this.handleChange}
+                                autoFocus={true}
+                            />
+                        <select value={this.state.newCategory} className="new-todo"
+                        onChange={this.handleCategoryChange}>
+                            <option value="">Select a Category</option>
+                            <option value="work">Work</option>
+                            <option value="personal">Personal</option>
+                            <option value="superpersonal">Super Personal</option>
+                        </select>
+
+                        </form>
+                    </header>
+                    {main}
+                    {category}
+                    {footer}
+                </div>
+            );
+        }
+    });
 
 	var model = new app.TodoModel('react-todos');
 
